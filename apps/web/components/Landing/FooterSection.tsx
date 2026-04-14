71167385feb0ae2fa5840f680557d56620a72eee
sprincessgenevieve <sprincessgenevieve@gmail.com>
2026-04-14 15:35:05 +0800

    [Feat] [Subscription] - created subscription section

---
+++ apps/web/components/Landing/FooterSection.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function FooterSection() {
+  return <div className="min-h-100 w-full bg-red-200">FooterSection</div>
+}
--- apps/web/components/Landing/SubscribeSection.tsx
+++ apps/web/components/Landing/SubscribeSection.tsx
@@ -1,12 +1,51 @@
 import React from "react"
 import SmartGradientCard from "../SmartGradientCard"
 import Input from "@workspace/ui/components/input"
+import { Button } from "@workspace/ui/components/button"
+import SectionTitle from "./SectionTitle"
 
 export default function () {
   return (
-    <div className="min-h-screen w-full">
+    <div className="relative flex min-h-[70vh] w-full">
+      <div className="z-30 flex h-full w-full flex-col items-center justify-center p-4">
+        <SectionTitle
+          title={"Stay Updated"}
+          desc={"Subscribe to get the latest tour deals and travel tips"}
+        ></SectionTitle>
+        <div className="flex w-full max-w-200 flex-col gap-4 rounded-3xl bg-cyan-100/30 bg-linear-0 p-8 shadow-2xl shadow-cyan-600/70 backdrop-blur-2xl">
+          <div className="flex gap-4 max-[670px]:flex-col">
+            <Input
+              className="w-full bg-white"
+              containerClassName="w-full"
+              label="First Name"
+            ></Input>
+            <Input
+              className="w-full bg-white"
+              placeholder=""
+              label="Last Name"
+              containerClassName="w-full"
+            ></Input>
+          </div>
+          <Input label="Email" className="bg-white"></Input>
+          <div className="flex gap-4 max-[670px]:flex-col">
+            <Input
+              className="w-full bg-white"
+              containerClassName="w-full"
+              label="Phone Number (Optional)"
+            ></Input>
+            <Input
+              className="w-full bg-white"
+              placeholder=""
+              label="Nationality (Optional)"
+              containerClassName="w-full"
+            ></Input>
+          </div>
+          <Button>Subscribe</Button>
+        </div>
+      </div>
+
       <div
-        className="absolute h-screen w-full"
+        className="absolute top-0 h-full w-full"
         style={{
           WebkitMaskImage:
             "linear-gradient(to bottom, transparent, black 20%, black 80%, transparent)",
@@ -15,14 +54,6 @@ export default function () {
         }}
       >
         <SmartGradientCard hoverable={false}></SmartGradientCard>
-      </div>
-      <div className="flex w-full flex-col items-center">
-        <div className="w-full max-w-200 rounded-3xl">
-          <div className="flex">
-            <Input></Input>
-            <Input></Input>
-          </div>
-        </div>
       </div>
     </div>
   )
